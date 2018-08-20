b1b47c9c9d85809f91a0eb2a4d557bedd23a9246
Stephen Williams <steve@icarus.com>
2018-08-20 09:35:36 -0700

    Add quickboot_gold3 and quickboot_silver3 commands.

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 CXX = g++ -std=c++11
 CXXFLAGS = -O -g -Wall
 
-all: quickboot_builder quickboot_builder3 quickboot_gold
+all: quickboot_builder quickboot_gold quickboot_builder3 quickboot_silver3 quickboot_gold3
 
 clean:
 	rm -f *.o *~
@@ -25,11 +25,25 @@ G = quickboot_gold.o read_bit_file.o test_image_compat.o extract_register_write.
 quickboot_gold: $G
 	$(CXX) $(CXXFLAGS) -o quickboot_gold $G
 
+S3 = quickboot_silver3.o read_bit_file.o replace_register_write.o
+
+quickboot_silver3: $(S3)
+	$(CXX) $(CXXFLAGS) -o quickboot_silver3 $(S3)
+
+G3 = quickboot_gold3.o read_bit_file.o replace_register_write.o disable_stream_crc.o
+
+quickboot_gold3: $(G3)
+	$(CXX) $(CXXFLAGS) -o quickboot_gold3 $(G3)
+
 quickboot_builder.o: quickboot_builder.cc read_bit_file.h extract_register_write.h replace_register_write.h test_image_compat.h disable_stream_crc.h write_to_mcs_file.h
 
 quickboot_builder3.o: quickboot_builder3.cc disable_stream_crc.h read_bit_file.h replace_register_write.h write_to_mcs_file.h
 
 quickboot_gold.o: quickboot_gold.cc read_bit_file.h replace_register_write.h test_image_compat.h disable_stream_crc.h
+
+quickboot_silver3.o: quickboot_silver3.cc read_bit_file.h replace_register_write.h
+
+quickboot_gold3.o: quickboot_gold3.cc read_bit_file.h replace_register_write.h disable_stream_crc.h
 
 read_bit_file.o:     read_bit_file.cc read_bit_file.h
 extract_register_write.o: extract_register_write.cc extract_register_write.h
---
+++ quickboot_silver3.cc
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2017 Picture Elements, Inc.
+ *    Stephen Williams (steve@icarus.com)
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/*
+ *  Take in a raw .bit file from Xilinx tools and convert it into a
+ *  .bit file that is ready for field installation into the flash.
+ *
+ *  NOTE: This program is NOT needed to prepare .bit file for input to
+ *  the quickboot_builder3 program. Only use this program if you are
+ *  NOT using the quickboot_builder3 program to prepare the silver image.
+ */
+
+# include  "read_bit_file.h"
+# include  "replace_register_write.h"
+# include  <vector>
+# include  <cstdio>
+# include  <cstdint>
+# include  <cstdlib>
+# include  <cstring>
+# include  <cassert>
+
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+      const char*path_out = 0;
+      const char*path_raw = 0;
+
+      for (int optarg = 1 ; optarg < argc ; optarg += 1) {
+	    if (strncmp(argv[optarg], "--output=",9) == 0) {
+		  path_out = argv[optarg] + 9;
+
+	    } else if (strncmp(argv[optarg], "--raw=",5) == 0) {
+		  path_raw = argv[optarg] + 5;
+
+	    } else {
+		  fprintf(stderr, "Unknown flag: %s\n", argv[optarg]);
+		  return -1;
+	    }
+      }
+
+      if (path_out == 0) {
+	    fprintf(stderr, "No output file? Please specify --output=<path>\n");
+	    return -1;
+      }
+
+      if (path_raw == 0) {
+	    fprintf(stderr, "No raw input file? Please specify --raw=<path>\n");
+	    return -1;
+      }
+
+	// Read the raw file, strip any header, and be ready.
+      FILE*fd_raw = fopen(path_raw, "rb");
+      if (fd_raw == 0) {
+	    fprintf(stderr, "Unable to open raw file: %s\n", path_raw);
+	    return -1;
+      }
+
+      fprintf(stdout, "Reading raw file: %s\n", path_raw);
+      fflush(stdout);
+      vector<uint8_t> vec_raw;
+      read_bit_file(vec_raw, fd_raw, 256+32);
+      if (vec_raw.size() == 0)
+	    return -1;
+
+      fclose(fd_raw);
+      fd_raw = 0;
+
+	// Edit the silver stream BSPI register value.
+      uint32_t old_BSPI = replace_register_write(vec_raw, 0x14, 0x0c);
+      fprintf(stdout, "BSPI (silver): 0x00000c (was: 0x%08x)\n", old_BSPI);
+
+      FILE*fd_out = fopen(path_out, "wb");
+      if (fd_out == 0) {
+	    fprintf(stderr, "Unable to open output file: %s\n", path_out);
+	    return -1;
+      }
+
+      size_t rc = fwrite(&vec_raw[0], 1, vec_raw.size(), fd_out);
+      assert(rc == vec_raw.size());
+
+      fclose(fd_out);
+
+      return 0;
+}
